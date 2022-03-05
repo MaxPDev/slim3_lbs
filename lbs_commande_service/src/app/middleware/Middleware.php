6a85d11770bfd48152baedd5432198e6e8776aaf
Maxime Piscaglia <maximepiscaglia@hotmail.com>
2022-03-05 19:09:30 +0100

    merge

---
+++ lbs_commande_service/src/app/middleware/Middleware.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace lbs\command\app\middleware;
+
+use \Slim\Container;
+
+use \Psr\Http\Message\ResponseInterface as Response;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+
+use lbs\command\app\models\Commande;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class Middleware
+{
+
+    private $container;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    public function checkToken(Request $req, Response $resp, callable $next): Response
+    {
+
+        // récupérer l'identifiant de cmmde dans la route et le token
+        $id = $req->getAttribute('route')->getArgument('id');
+        $token = $req->getQueryParam('token', null);
+        // vérifier que le token correspond à la commande
+        try {
+            Commande::where('id', '=', $id)
+                ->where('token', '=', $token)
+                ->firstOrFail();
+        } catch (ModelNotFoundException $e) {
+            // générer une erreur
+            return $resp;
+        };
+
+        return $resp;
+    }
+
+    // //todo faire un middleware token avec une methode check
+    // $token = null
+    // $token = rq get queryparal
+    // if is_null $token
+    // $api_header = rq get header X-commande_api-token
+    // $token isset 
+}
--- lbs_commande_service/src/app/routes/routes_td.php
+++ lbs_commande_service/src/app/routes/routes_td.php
@@ -2,21 +2,38 @@
 
 use lbs\command\app\controller\Commande_Controller;
 use lbs\command\app\controller\Commande_Item_Controller;
+use lbs\command\app\middleware\Middleware;
 
 //* TD1 & TD2
 
 // Route pour une commande
-$app->get('/td/commandes/{id}[/]', Commande_Controller::class . ':getCommande')->setName('getCommande');
+$app->get('/td/commandes/{id}[/]', Commande_Controller::class . ':getCommande')
+    ->setName('getCommande');
+// ->add(Middleware::class . ':checkToken');
 
 // Route pour toute les commandes
-$app->get('/td/commandes[/]', Commande_Controller::class . ':getAllCommande')->setName('getAllCommande');
+$app->get('/td/commandes[/]', Commande_Controller::class . ':getAllCommande')
+    ->setName('getAllCommande');
 
 
 //* TD3
 
-$app->put('/td/commandes/{id}/items[/]', Commande_Controller::class . ':putCommande')->setName('putCommande');
+$app->put('/td/commandes/{id}/items[/]', Commande_Controller::class . ':putCommande')
+    ->setName('putCommande');
 
 
 //* TD4
 
-$app->get('/td/commandes/{id}/items[/]', Commande_Item_Controller::class . ':getItems')->setName('getCommandesItems');
+$app->get('/td/commandes/{id}/items[/]', Commande_Item_Controller::class . ':getItems')
+    ->setName('getCommandesItems');
+
+//! correctin : getCommandItem, class Token::class : check pour middleware
+
+//* TD5
+
+// Création d'une commande
+$app->post('/td/commandes[/]', Commande_Controller::class . ':createCommande')
+    ->setName('createCommande');
+
+
+    //! token a mettre sur item, commande et paiement, et replacecommant (put)
