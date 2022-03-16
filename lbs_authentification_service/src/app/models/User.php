580df0ce01ed1032fd06af0fcc3eb5417e771bc0
Maxime Piscaglia <maximepiscaglia@hotmail.com>
2022-03-17 00:00:22 +0100

    trying again...

--- lbs_authentification_service/src/app/models/User.php
+++ lbs_authentification_service/src/app/models/User.php
@@ -7,11 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     protected $table = 'user';
-
     protected $primaryKey = 'id';
     public $incrementing = true;
+    public $timestamps = true;
 
     protected $fillable = array(
-        'id', 'email', 'username', 'password', 'refresh_token', 'level', 'created_at', 'updated_at'
+        'id', 'email', 'username', 'passwd', 'refresh_token', 'level', 'created_at', 'updated_at'
     );
 }
