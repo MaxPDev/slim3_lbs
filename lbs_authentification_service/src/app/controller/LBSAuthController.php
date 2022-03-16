31fff834e6e75d462ce6dc5377d89e0a5b22417e
Maxime Piscaglia <maximepiscaglia@hotmail.com>
2022-03-16 21:48:58 +0100

    trying to make auth work...

--- lbs_authentification_service/src/app/controller/LBSAuthController.php
+++ lbs_authentification_service/src/app/controller/LBSAuthController.php
@@ -11,8 +11,10 @@ namespace lbs\auth\app\controller;
 
 
 use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 use Firebase\JWT\ExpiredException;
 use Firebase\JWT\SignatureInvalidException;
+use Firebase\JWT\BeforeValidException;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use lbs\auth\app\models\User;
@@ -85,32 +87,42 @@ class LBSAuthController
             return Writer::json_error($rs, 401, 'Erreur authentification');
         }
 
+        //*  "iss" : "issuer", identifie l'émetteur du token
+        //*  "sub" : "subject", le sujet du token
+        //*  "aud" : "audience", destinataires du token – le serveur
+        //* r       recevant un token doit vérifier qu'il lui est bien destiné
+        //*  "iat" : "issued at", date d'émission du token
+        //*  "exp" : "expires", date d'expiration du token
+        //*  "nbf" : "not before", date de validité du token
+        //*  "jti" : "jwt id", identificateur unique du token
 
-        $secret = $this->container->settings['secret'];
-        $token = JWT::encode(
-            [
-                'iss' => 'http://api.auth.local/auth',
-                'aud' => 'http://api.backoffice.local',
-                'iat' => time(),
-                'exp' => time() + (12 * 30 * 24 * 3600),
-                'upr' => [
-                    'email' => $user->email,
-                    'username' => $user->username,
-                    'level' => $user->level
-                ]
-            ],
-            $secret,
-            'HS512'
-        );
+        // $secret = $this->container->settings['secret'];
+        // $token = JWT::encode(
+        //     [
+        //         'iss' => 'http://api.auth.local/auth',
+        //         'aud' => 'http://api.backoffice.local',
+        //         'iat' => time(),
+        //         'exp' => time() + (12 * 30 * 24 * 3600),
+        //         'upr' => [
+        //             'email' => $user->email,
+        //             'username' => $user->username,
+        //             'level' => $user->level
+        //         ]
+        //     ],
+        //     $secret,
+        //     'HS512'
+        // );
 
-        $user->refresh_token = bin2hex(random_bytes(32));
-        $user->save();
-        $data = [
-            'access-token' => $token,
-            'refresh-token' => $user->refresh_token
-        ];
+        // $user->refresh_token = bin2hex(random_bytes(32));
+        // $user->save();
+        // $data = [
+        //     'access-token' => $token,
+        //     'refresh-token' => $user->refresh_token
+        // ];
 
-        return Writer::json_output($rs, 200, $data);
+        // return Writer::json_output($rs, 200, $data);
+        $rs->getBody()->write("Succeded");
+        return writer::json_output($rs, 200);
     }
 
     public function check(Request $req, Response $resp, $args): Response
